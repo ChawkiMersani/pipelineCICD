29d3a62737d1020b8f715831b55c08793cdc3f2d
ChawkiMersani <100194333+ChawkiMersani@users.noreply.github.com>
2024-06-21 10:13:51 +0100

    change and deploy

--- src/main/java/com/Guidewire/Monitoring/Controllers/AccountController.java
+++ src/main/java/com/Guidewire/Monitoring/Controllers/AccountController.java
@@ -13,7 +13,7 @@ import java.util.List;
 
 @CrossOrigin(origins = "http://localhost:51176")
 @RestController
-@RequestMapping("/accounts")
+@RequestMapping("/accountsss")
 
 public class AccountController {
 
